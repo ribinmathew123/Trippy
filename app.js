4199729acd0af5ad7d87845dbfa701cefbed3f36
ribinmathew <ribinmathew4u@gmail.com>
2023-07-09 23:23:24 +0530

    server updation

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5173',"*","http://www.ribin.site/"]
+    origin: ['http://localhost:5173',"https://www.ribin.site/"]
   })
 );
 
